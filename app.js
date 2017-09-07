d3fd8e5cbf442288202881720d8cd0891f684fd8
mandafae <mandafae5@gmail.com>
2017-09-07 14:24:50 -0500

    refactored

--- app.js
+++ app.js
@@ -1,97 +1,8 @@
-// $('.search input[type=submit]').click((e) => {
-  // e.preventDefault();
-
-/* ZOMATO API CALL. Decided not to use when I got Yelp API call working but keeping the code in case I need it. */
-//   let searchText = $('#location').val();
-//   let locationType = "city";
-//   let locationId = 1;
-//   // Perform API call to search locations and get id
-//   $.ajax({
-//     url: (`https://developers.zomato.com/api/v2.1/locations?query=${searchText}`),
-//     // ('https://developers.zomato.com/api/v2.1/search?entity_id=278&entity_type=city&q=pet-friendly=1'),
-//     headers: {'user-key': '733d7e1ab9b34bd7a2e074d63aa4bef1'},
-//     dataType: 'json'
-//   }).done(function(data) {
-//     // Display results of location search
-//     // When user makes a selection, set it as the location to search and take user to results page
-//     let locationResults = document.createElement('div');
-//     document.getElementsByClassName('searchForm')[0].append(locationResults);
-//     let friendlyText = document.createElement('p');
-//     friendlyText.innerText = "Please select a location from the list below:"
-//     locationResults.append(friendlyText);
-//     let suggestions = data.location_suggestions;
-//     for (let i=0; i<suggestions.length; i++) {
-//       let location = document.createElement('p');
-//       location.innerHTML = `${suggestions[i].title}`;
-//       locationResults.append(location);
-//       $(location).click(() => {
-//         locationType = data.location_suggestions[0].entity_type;
-//         locationId = data.location_suggestions[0].entity_id;
-//         //window.location.href= 'results.html';
-//         $.ajax({
-//           url:  (`https://developers.zomato.com/api/v2.1/search?entity_id=${locationId}&entity_type=${locationType}&q=pet-friendly=1`),
-//           headers: {'user-key': '733d7e1ab9b34bd7a2e074d63aa4bef1'},
-//           dataType: 'json'
-//         }).done(function(data) {
-//           console.log(locationType)
-//           console.log(locationId);
-//           console.log(data);
-//           for (let i=0; i<data.restaurants.length; i++) {
-//             let result = document.createElement('div');
-//             result.innerHTML = `<p>${data.restaurants[i].restaurant.name}<br>
-//                                     ${data.restaurants[i].restaurant.location.locality}<br>
-//                                     ${data.restaurants[i].restaurant.cuisines}</p>`;
-//             locationResults.append(result);
-//         }
-//         });
-//       });
-//     };
-//   })
-// });
-
-//
-  // YELP API CALL
-  // Get input text to use for search
-//   let searchText = $('#location').val();
-//   // Define the settings for the API call
-//   var settings = {
-//     "async": true,
-//     "crossDomain": true,
-//     "url": `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=dog%20friendly&categories=restaurants,bars&open_now=true&sort_by=distance&location=${searchText}`,
-//     "method": "GET",
-//     "headers": {
-//       "authorization": "Bearer 80LIgTKphcU7oWrMtt4e9TahnIrn-P3sMkhDw1B3CW25GdSHZB4-PU-dM_hRIwx3AzT3SUPoXo0fRtNadtudHoxBrjGTZr_Wgavv6fqQ5ZRoH79m9HaXPBjrnX-pWXYx",
-//       "cache-control": "no-cache",
-//       "postman-token": "3f23d8c3-ce48-a224-50c0-14b9094948fc"
-//     }
-//   }
-//
-//   // Use AJAX to perform API call
-//   $.ajax(settings).done(function (response) {
-//     let results = response.businesses;
-//     // Display results
-//     results.forEach(function(business) {
-//       let businessInfo = document.createElement('div');
-//       let businessName = business.name;
-//       let businessImg = business.image_url;
-//       let businessAddress = `${business.location.display_address[0]}, ${business.location.display_address[1]}`;
-//       businessInfo.innerHTML = `<img src=${businessImg} style='width: 10%; height: 10%'><h4>${businessName}</h4><p>${businessAddress}</p>`;
-//       $('.searchForm').append(businessInfo);
-//     });
-//     // window.location.href = 'results.html';
-//   })
-//   .fail(function() {
-//     let errorMessage = document.createElement('p');
-//     errorMessage.innerText = "We're sorry. Your search did not return any results.";
-//     $('.searchForm').append(errorMessage);
-//   });
-// });
-
 // Draw divs for decorative left border
 function drawLeftBorder (){
   let leftBorderContainer = document.getElementsByClassName('leftBorder')[0];
   let windowHeight = $(window).height();
-  for (let i=0; i<(windowHeight/190); i++) {
+  for (let i=0; i<(windowHeight/200); i++) {
     let circle = document.createElement('div');
     circle.className = "leftBorderCircles";
     if (i % 2 === 0) {
@@ -109,7 +20,7 @@ drawLeftBorder();
 function drawRightBorder (){
   let rightBorderContainer = document.getElementsByClassName('rightBorder')[0];
   let windowHeight = $(window).height();
-  for (let i=0; i<(windowHeight/190); i++) {
+  for (let i=0; i<(windowHeight/200); i++) {
     let circle = document.createElement('div');
     circle.className = "rightBorderCircles";
     if (i % 2 === 0) {
@@ -122,3 +33,123 @@ function drawRightBorder (){
   };
 };
 drawRightBorder();
+
+searchText = '';
+
+// Create function to initialize map
+function initMap(business, latlng) {
+  let directionsDisplay = new google.maps.DirectionsRenderer();
+  let location = latlng;
+  let map = new google.maps.Map(document.getElementById(business), {
+    center: location,
+    zoom: 15
+  });
+  directionsDisplay.setMap(map);
+
+  // Set origin, destination, and travel mode for route calculation
+  let start = window.searchText;
+  let end = latlng;
+  let request = {
+    origin: start,
+    destination: end,
+    travelMode: 'DRIVING'
+  };
+
+  // Pass directions settings to the Google directions service
+  let directionsService = new google.maps.DirectionsService();
+  directionsService.route(request, function(result, status) {
+    if (status == 'OK') {
+      // Display the route
+      directionsDisplay.setDirections(result);
+    }
+  });
+};
+
+// Add click event for search
+$('.search input[type=submit]').click((e) => {
+  e.preventDefault();
+  $('.searchForm').hide();
+  $('.photo').hide();
+
+  // Get input text to use for search
+  window.searchText = $('#location').val();
+
+  // Define the settings for the API call
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=dog+friendly&categories=restaurants,bars&open_now=true&sort_by=distance&location=${window.searchText}`,
+    "method": "GET",
+    "headers": {
+      "authorization": "Bearer 80LIgTKphcU7oWrMtt4e9TahnIrn-P3sMkhDw1B3CW25GdSHZB4-PU-dM_hRIwx3AzT3SUPoXo0fRtNadtudHoxBrjGTZr_Wgavv6fqQ5ZRoH79m9HaXPBjrnX-pWXYx",
+      "cache-control": "no-cache",
+      "postman-token": "3f23d8c3-ce48-a224-50c0-14b9094948fc"
+    }
+  }
+
+  // Use AJAX to perform Yelp API call
+  $.ajax(settings).done(function (response) {
+    let results = response.businesses;
+
+    // Create div to hold all search results
+    let resultsDiv = document.createElement('div');
+    $(resultsDiv).addClass('searchResults');
+    $('main').append(resultsDiv);
+
+    // Display results
+    results.forEach(function(business) {
+      // Create each business result div
+      let businessInfo = document.createElement('div');
+      let businessName = business.name;
+      let businessAddress = `${business.location.display_address[0]}, ${business.location.display_address[1]}`;
+      businessInfo.innerHTML = `<h4>${businessName}</h4><p>${businessAddress}</p>`;
+      $(businessInfo).addClass('result');
+
+      // Create each details div
+      let details = document.createElement('div');
+      let businessImg = business.image_url;
+      let category = business.categories[0].title;
+      let phone = business.display_phone;
+      let price = business.price;
+      let geo = {lat: business.coordinates.latitude, lng: business.coordinates.longitude};
+      let mapDiv = document.createElement('div');
+      details.innerHTML = `<img src=${businessImg} style='width: auto; height: auto; max-width: 150px; max-height: 100px'><p>Category: ${category}<br>Phone number: ${phone}<br>Average price: ${price}</p>`;
+      $(details).addClass('detail');
+      $(mapDiv).attr({id: businessName, class: 'map'});
+
+      // Add map div to details
+      $(details).append(mapDiv);
+
+      // Add divs to DOM
+      $(businessInfo).append(details);
+      // Hide details
+      $(details).hide();
+      $('.searchResults').append(businessInfo);
+      // Add hover event to toggle details
+      $(businessInfo).hover(function() {
+        $(details).toggle();
+        // Call map function to display map
+        initMap(businessName, geo);
+      })
+    })
+
+    // Re-draw borders based on length of content in search results
+    drawLeftBorder();
+    drawRightBorder();
+  })
+  // Error handling
+  .fail(function() {
+    let errorMessage = document.createElement('p');
+    errorMessage.innerText = "We're sorry. Your search did not return any results.";
+    $('.searchResults').append(errorMessage);
+  });
+});
+
+$('#logo').click((e) => {
+  e.preventDefault();
+  $('#location').val('');
+  $('.searchResults').remove();
+  window.searchText = '';
+  $('.searchForm').show();
+  $('.photo').show();
+})
